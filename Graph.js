60e35b2d27b433c66e0a366a5a004cea0e3c2997
dee-diaz <diazthediablo@yahoo.com>
2025-12-12 16:08:18 -0300

    Create Graph class with adjacency list

--- Graph.js
+++ Graph.js
@@ -3,13 +3,34 @@ class Graph {
     this.adjList = new Map();
   }
 
-  addVertex(vertex) {}
+  addVertex(vertex) {
+    if (!this.adjList.has(vertex)) {
+      this.adjList.set(vertex, []);
+    } else {
+      throw new Error(`Vertex ${vertex} already exists.`);
+    }
+  }
 
-  addEdge(vertex1, vertex2) {}
+  addEdge(vertex1, vertex2) {
+    if (this.adjList.has(vertex1) && this.adjList.has(vertex2)) {
+      this.adjList.get(vertex1).push(vertex2);
+      this.adjList.get(vertex2).push(vertex1);
+    } else {
+      throw new Error("One or both vertices do not exist.");
+    }
+  }
 
-  getNeighboors(vertex) {}
+  getNeighbors(vertex) {
+    return this.adjList.get(vertex);
+  }
 
-  hasEdge(vertex1, vertex2) {}
-
-  printGraph() {}
+  hasEdge(vertex1, vertex2) {
+    if (this.adjList.has(vertex1) && this.adjList.has(vertex2)) {
+      return this.adjList.get(vertex1).includes(vertex2);
+    } else {
+      throw new Error("One or both vertices do not exist.");
+    }
+  }
 }
+
+export default Graph;
