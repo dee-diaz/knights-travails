afe60c8de27755b41f8c87275472b2451790df27
dee-diaz <diazthediablo@yahoo.com>
2025-12-12 12:54:51 -0300

    Basic Graph class structure

---
+++ Graph.js
@@ -0,0 +1,15 @@
+class Graph {
+  constructor() {
+    this.adjList = new Map();
+  }
+
+  addVertex(vertex) {}
+
+  addEdge(vertex1, vertex2) {}
+
+  getNeighboors(vertex) {}
+
+  hasEdge(vertex1, vertex2) {}
+
+  printGraph() {}
+}
