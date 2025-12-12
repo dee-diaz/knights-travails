15e93d93bf275d8baa77b4997471e325bee8e1ae
dee-diaz <diazthediablo@yahoo.com>
2025-12-12 16:59:49 -0300

    Add driver code to build graph

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-import Graph from "./Graph";
+import Graph from "./Graph.js";
+
+const graph = new Graph();
+const vertices = createVertices();
+
+function createVertices(boardSize = 8) {
+  let arr = [];
+
+  // Create rows
+  for (let row = 0; row < boardSize; row++) {
+    for (let col = 0; col < boardSize; col++) {
+      arr.push(`${row},${col}`);
+    }
+  }
+
+  return arr;
+}
+
+function initGraph() {
+  vertices.forEach((vertex) => graph.addVertex(vertex));
+}
+
+initGraph();
